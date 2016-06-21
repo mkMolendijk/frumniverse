6e3036a798ae3ef158e667206ed5432ddb9b119e
mkMolendijk <markmolendijk@Marks-MBP.Home>
2016-06-22 00:55:00 +0200

    Added six character words for the fourth level

--- dist/js/main.js
+++ dist/js/main.js
@@ -404,7 +404,31 @@ var LevelFour = (function (_super) {
     __extends(LevelFour, _super);
     function LevelFour() {
         _super.call(this, 4);
+        this.enemyNames = ["ankers", "bessen", "cowboy", "douche", "ekster", "filmen", "gevaar", "heksen", "ijsjes", "jeetje", "keuken", "lusten", "mazzel", "najaar",
+            "oranje", "punten", "quotes", "reuzen", "schaap", "tempel", "uurtje", "vijver", "windje", "zinken", "zwaard"];
+        this.platform = new Platform(this, 4);
+        this.platform.width = 1129;
+        this.platform.height = 48;
+        this.platform.posX = (window.innerWidth - this.platform.width) / 2;
+        this.platform.posY = (window.innerHeight - (this.platform.height + 100));
+        this.enemySize = 4;
+        this.platform.draw();
+        this.rock = new Platform(this, 21);
+        this.rock.width = 554;
+        this.rock.height = 164;
+        this.rock.posX = this.platform.posX + 575;
+        this.rock.posY = this.platform.posY - this.rock.height;
+        this.rock.draw();
+        this.dog = new Dog(this, this.platform);
+        this.timer = setInterval(this.createEnemy.bind(this), 2000);
     }
+    LevelFour.prototype.createEnemy = function () {
+        this.enemies.push(new Enemy(this, 196, 221));
+    };
+    LevelFour.prototype.remove = function () {
+        clearInterval(this.timer);
+        _super.prototype.remove.call(this);
+    };
     return LevelFour;
 }(Level));
 var LevelThree = (function (_super) {
