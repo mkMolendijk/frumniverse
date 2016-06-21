c6ec83087fca6b0ef023790d1e95778d963afd29
SRPater <srpater@gmail.com>
2016-06-21 23:15:24 +0200

    Made level three

--- dev/levels/three.ts
+++ dev/levels/three.ts
@@ -1,8 +1,48 @@
+/// <reference path="../platform.ts" />
+
 /**
  * LevelThree
  */
-class LevelThree extends Level{
+class LevelThree extends Level {
+    
+    private timer: number;
+    private door: Platform;
+
     constructor() {
         super(3);
+
+        this.enemyNames = ["aapje", "baard", "cavia", "druif", "elfje", "fluit", "geest", "hemel", "ijsje", "jagen", "klein", "lucht", "mesje", "nieuw",
+            "ouder", "paard", "kwark", "ritje", "steen", "teken", "uilen", "visje", "weten", "zebra", "zwaai"];
+        
+        this.platform = new Platform(this, 3);
+        this.platform.width = window.innerWidth;
+        this.platform.height = 1;
+        this.platform.posX = 0;
+        this.platform.posY = window.innerHeight - 10;
+
+        this.platform.draw();
+
+        this.door = new Platform(this, 31);
+        this.door.width = 68;
+        this.door.height = 71;
+        this.door.posX = (window.innerWidth - this.door.width) / 2;
+        this.door.posY = window.innerHeight - this.door.height;
+
+        this.enemySize = 5;
+
+        this.door.draw();
+
+        this.dog = new Dog(this, this.platform);
+
+        this.timer = setInterval(this.createEnemy.bind(this), 2000);
+    }
+
+    private createEnemy() : void{
+        this.enemies.push(new Enemy(this, 130, 110));
+    }
+
+    public remove() {
+        clearInterval(this.timer);
+        super.remove();
     }
 }
--- dist/js/main.js
+++ dist/js/main.js
@@ -411,7 +411,31 @@ var LevelThree = (function (_super) {
     __extends(LevelThree, _super);
     function LevelThree() {
         _super.call(this, 3);
+        this.enemyNames = ["aapje", "baard", "cavia", "druif", "elfje", "fluit", "geest", "hemel", "ijsje", "jagen", "klein", "lucht", "mesje", "nieuw",
+            "ouder", "paard", "kwark", "ritje", "steen", "teken", "uilen", "visje", "weten", "zebra", "zwaai"];
+        this.platform = new Platform(this, 3);
+        this.platform.width = window.innerWidth;
+        this.platform.height = 1;
+        this.platform.posX = 0;
+        this.platform.posY = window.innerHeight - 10;
+        this.platform.draw();
+        this.door = new Platform(this, 31);
+        this.door.width = 68;
+        this.door.height = 71;
+        this.door.posX = (window.innerWidth - this.door.width) / 2;
+        this.door.posY = window.innerHeight - this.door.height;
+        this.enemySize = 5;
+        this.door.draw();
+        this.dog = new Dog(this, this.platform);
+        this.timer = setInterval(this.createEnemy.bind(this), 2000);
     }
+    LevelThree.prototype.createEnemy = function () {
+        this.enemies.push(new Enemy(this, 130, 110));
+    };
+    LevelThree.prototype.remove = function () {
+        clearInterval(this.timer);
+        _super.prototype.remove.call(this);
+    };
     return LevelThree;
 }(Level));
 var LevelTwo = (function (_super) {
